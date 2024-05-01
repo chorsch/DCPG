4efba1c213cd4d3807a631cdf4972728f020b1ff
Max Weltevrede <maxweltevrede@gmail.com>
2024-05-01 16:15:43 +0200

    Add env wrapper that allows for getting and setting states

--- dcpg/envs.py
+++ dcpg/envs.py
@@ -62,7 +62,45 @@ class VecPyTorchProcgen(VecEnvWrapper):
             "cliprew": cliprew,
         }
         return norm_infos
+    
 
+class VecSettableProcgen(VecEnvWrapper):
+    def __init__(self, venv: VecEnv):
+        """
+        Environment wrapper that defines a function that allows for getting and setting states in a Procgen VecEnv
+        """
+        super(VecSettableProcgen, self).__init__(venv)
+
+    def get_states(self):
+        base_env = self.venv
+        unwrapped = not hasattr(base_env, 'venv')
+        while not unwrapped:
+            base_env = base_env.venv
+            unwrapped = not hasattr(base_env, 'venv')
+        if hasattr(base_env, 'env'):
+            return base_env.env.get_state()
+        
+    def set_states(self, states, env_indices):
+        new_states = self.get_states()
+        for c, v in enumerate(env_indices):
+            new_states[v] = states[c]
+
+        base_env = self.venv
+        unwrapped = not hasattr(base_env, 'venv')
+        while not unwrapped:
+            base_env = base_env.venv
+            unwrapped = not hasattr(base_env, 'venv')
+        if hasattr(base_env, 'env'):
+            base_env.env.set_state(new_states)
+
+    def reset(self):
+        return self.venv.reset()
+    
+    def step_wait(self):
+        return self.venv.step_wait()
+    
+    def step_async(self, actions: Tensor):
+        self.venv.step_async(actions)
 
 def make_envs(
     num_envs: int,
@@ -84,5 +122,6 @@ def make_envs(
     envs = VecMonitor(envs, filename=None, keep_buf=100)
     envs = VecNormalize(envs, ob=False, ret=normalize_reward)
     envs = VecPyTorchProcgen(envs, device)
+    envs = VecSettableProcgen(envs)
 
     return envs
