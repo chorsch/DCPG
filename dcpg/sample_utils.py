4e93d58260ad79f7843b98601cd832f71b741ad2
Max Weltevrede <maxweltevrede@gmail.com>
2024-06-13 12:08:12 +0200

    Pure exploration through random action selection

--- dcpg/sample_utils.py
+++ dcpg/sample_utils.py
@@ -12,18 +12,13 @@ from dcpg.rnd import RandomNetworkDistillationState, RandomNetworkDistillationSt
 def sample_episodes(
     envs: VecPyTorchProcgen,
     rollouts: RolloutStorage,
-    pure_rollouts: RolloutStorage,
     obs: torch.Tensor,
     levels: torch.Tensor,
     pure_expl_steps_per_env: np.ndarray,
     episode_steps: np.ndarray,
     ac: PPOModel,
-    pure_expl_ac: PPOModel,
     num_pure_expl_steps: int,
     num_processes: int,
-    rnd: Union[RandomNetworkDistillationState, RandomNetworkDistillationStateAction],
-    rnd_next_state: bool,
-    normalise: bool,
 ) -> Tuple[List[float], torch.Tensor, torch.Tensor, int]:
     """
     Sample episodes
@@ -42,20 +37,18 @@ def sample_episodes(
             if sum(normal_inds) > 0:
                 normal_action, normal_action_log_prob, normal_value = ac.act(obs[normal_inds])
                 if sum(pure_expl_inds) > 0:
-                    pure_action, pure_action_log_prob, pure_value = pure_expl_ac.act(obs[pure_expl_inds])
+                    pure_action = torch.randint(0, envs.action_space.n, size=(obs[pure_expl_inds].shape[0],1), device=device)
                 else:
                     # cannot call act with empty observation tensor
                     # so create our own empty results
-                    pure_action = torch.zeros((0, *normal_action.shape[1:]), dtype=normal_action.dtype, device=normal_action.device)
-                    pure_action_log_prob = torch.zeros((0, *normal_action_log_prob.shape[1:]), dtype=normal_action_log_prob.dtype, device=normal_action_log_prob.device)
-                    pure_value = torch.zeros((0, *normal_value.shape[1:]), dtype=normal_value.dtype, device=normal_value.device)
+                    pure_action = torch.zeros((0, *normal_action.shape[1:]), dtype=normal_action.dtype, device=device)
             else:
-                pure_action, pure_action_log_prob, pure_value = pure_expl_ac.act(obs[pure_expl_inds])
+                pure_action = torch.randint(0, envs.action_space.n, size=(obs[pure_expl_inds].shape[0],1), device=device)
                 # cannot call act with empty observation tensor
                 # so create our own empty results
-                normal_action = torch.zeros((0, *pure_action.shape[1:]), dtype=pure_action.dtype, device=pure_action.device)
-                normal_action_log_prob = torch.zeros((0, *pure_action_log_prob.shape[1:]), dtype=pure_action_log_prob.dtype, device=pure_action_log_prob.device)
-                normal_value = torch.zeros((0, *pure_value.shape[1:]), dtype=pure_value.dtype, device=pure_value.device)
+                normal_action = torch.zeros((0, *pure_action.shape[1:]), dtype=pure_action.dtype, device=device)
+                normal_action_log_prob = torch.zeros((0, 1), dtype=torch.float32, device=device)
+                normal_value = torch.zeros((0, 1), dtype=torch.float32, device=device)
         
         action = torch.zeros((num_processes, 1), dtype=normal_action.dtype, device=device)
         action[normal_inds] = normal_action
@@ -67,7 +60,7 @@ def sample_episodes(
         pure_masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done]).to(device)
         # Set mask to 0 for the last pure exploration step of the episode
         pure_masks[episode_steps == pure_expl_steps_per_env-1] = torch.FloatTensor([0.0]).to(device)
-        next_levels = torch.LongTensor([info["level_seed"] for info in infos]).to(device)
+        next_levels = torch.LongTensor([0 for info in infos]).to(device)
         reward = reward.to(device)
 
         # update episodic step count
@@ -77,16 +70,8 @@ def sample_episodes(
         # sample new number of pure exploration steps to perform
         pure_expl_steps_per_env[done] = np.random.randint(0, num_pure_expl_steps+1 , size=sum(done))
 
-        # Train the RND loss
-        if sum(pure_expl_inds) > 0:
-            if rnd_next_state:
-                rnd.observe(obs[pure_expl_inds])
-            else:
-                rnd.observe(obs[pure_expl_inds], pure_action)
-
         # Insert obs, action and reward into rollout storage
         rollouts.insert(obs[normal_inds], next_obs[normal_inds], normal_action, normal_action_log_prob, reward[normal_inds], normal_value, masks[normal_inds], levels[normal_inds], next_levels[normal_inds], normal_inds)
-        pure_rollouts.insert(obs[pure_expl_inds], next_obs[pure_expl_inds], pure_action, pure_action_log_prob, reward[pure_expl_inds], pure_value, pure_masks[pure_expl_inds], levels[pure_expl_inds], next_levels[pure_expl_inds], pure_expl_inds)
 
         # Track episode info
         for info in infos:
@@ -96,9 +81,5 @@ def sample_episodes(
         obs = next_obs
         levels = next_levels
         num_normal_steps += sum(normal_inds)
-
-    
-    # Update reward to include the intrinsic reward
-    pure_rollouts.update_rewards(rnd, rnd_next_state, normalise)
 
     return episode_rewards, next_obs, next_levels, num_normal_steps
